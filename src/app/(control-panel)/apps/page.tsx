c83f4547489a6960ae7f58e9ba1937856fafc499
TerryAtRocketVentures <68800619+terryjiang2020@users.noreply.github.com>
2026-02-16 12:36:36 +1300

    AH-124-5-2: remove unused mock apis and idle components

--- src/app/(control-panel)/apps/page.tsx
+++
@@ -1,8 +0,0 @@
-import { redirect } from 'next/navigation';
-
-function AppsPage() {
-	redirect(`/apps/academy`);
-	return null;
-}
-
-export default AppsPage;
--- src/app/(control-panel)/dashboards/page.tsx
+++
@@ -1,8 +0,0 @@
-import { redirect } from 'next/navigation';
-
-function DashboardsPage() {
-	redirect(`/test-results`);
-	return null;
-}
-
-export default DashboardsPage;
